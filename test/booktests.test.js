d07a98b526a6e7ec1eb78588fff9a83eef3abb20
Fidelis Ojeah <fidelis.ojeah@gmail.com>
2017-08-10 20:24:42 +0100

    push to include tests

---
+++ test/booktests.test.js
@@ -0,0 +1,267 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+// import supertest from 'supertest';
+
+import app from '../app';
+
+
+// const app = require('../app');
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+const should = chai.should();
+
+
+chai.use(chaiHttp);
+describe('POST /api/v1/authors version 1', () => {
+  before((done) => {
+    chai.request(app).delete('/api/v1/books').end(done);
+  });
+  it('should respond with a 200 incomplete details', (done) => {
+    chai.request(app)
+      .post('/api/v1/authors')
+      .send({
+        authorFName: 'Armanda', // no Author Last name
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure errors
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('unsuccessful');
+        res.body.message.should.eql('incomplete details');
+        done();
+      });
+  });
+  it('should respond with a 200 incomplete details', (done) => {
+    chai.request(app)
+      .post('/api/v1/authors')
+      .send({
+        authorLName: 'Righetti', // no Author First name
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure errors
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('unsuccessful');
+        res.body.message.should.eql('incomplete details');
+        done();
+      });
+  });
+  it('should respond with a 201 success', (done) => {
+    chai.request(app)
+      .post('/api/v1/authors')
+      .send({
+        authorFName: 'George',
+        authorLName: 'Martin',
+        authorDOB: '1948-09-20',
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure errors
+        res.status.should.equal(201);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('success');
+        res.body.data.AuthorName.should.eql('George Martin');
+        done();
+      });
+  });
+  it('should respond with a 201 success', (done) => {
+    chai.request(app)
+      .post('/api/v1/authors')
+      .send({
+        authorFName: 'Joanne',
+        authorLName: 'Rowling',
+        authorDOB: '1965-07-31',
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure errors
+        res.status.should.equal(201);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('success');
+        res.body.data.AuthorName.should.eql('Joanne Rowling');
+        done();
+      });
+  });
+});
+
+describe('POST /api/v1/books version 1 Create Books', () => {
+  it('should respond with a 200 incomplete details', (done) => {
+    chai.request(app)
+      .post('/api/v1/books')
+      .send({
+        authorId: 1, // incomplete book details
+      })
+      .end((err, res) => {
+        should.not.exist(err);
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('unsuccessful');
+        res.body.message.should.eql('incomplete Book Details!!');
+        done();
+      });
+  });
+  it('should respond with a 200 incomplete details', (done) => {
+    chai.request(app)
+      .post('/api/v1/books')
+      .send({
+        bookName: 'A Game of Thrones', // incomplete book details
+        authorId: 1,
+      })
+      .end((err, res) => {
+        should.not.exist(err);
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('unsuccessful');
+        res.body.message.should.eql('incomplete Book Details!!');
+        done();
+      });
+  });
+  it('should respond with a 201 success', (done) => {
+    chai.request(app)
+      .post('/api/v1/books')
+      .send({
+        bookName: 'A Game of Thrones',
+        bookISBN: '0553103547',
+        desc: 'A tail about the seven kingdoms',
+        pubYear: '1996',
+        authorId: 1,
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure errors
+        res.status.should.equal(201);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('success');
+        res.body.data.bookName.should.eql('A Game of Thrones');
+        done();
+      });
+  });
+  it('should respond with a 201 success', (done) => {
+    chai.request(app)
+      .post('/api/v1/books')
+      .send({
+        bookName: 'Harry Potter and the Prisoner of Azkaban',
+        bookISBN: '0747542155',
+        desc: 'Harry is back at the Dursleys, where he sees on Muggle television that a prisoner named Sirius Black has escaped',
+        pubYear: '1999',
+        authorId: 2,
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure errors
+        res.status.should.equal(201);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('success');
+        res.body.data.bookName.should.eql('Harry Potter and the Prisoner of Azkaban');
+        done();
+      });
+  });
+});
+describe('put /api/v1/books version 1 Modify Books', () => {
+  it('should respond with a 400 invalid book Id (number)', (done) => {
+    chai.request(app)
+      .put('/api/v1/books/a')
+      .send({
+        bookName: 'A Game of Thrones',
+        bookISBN: '0553103547',
+      })
+      .end((err, res) => {
+        should.exist(err);
+        res.status.should.equal(400);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('none');
+        res.body.message.should.eql('invalid number');
+        done();
+      });
+  });
+  it('should respond with a 200 invalid book Id', (done) => {
+    chai.request(app)
+      .put('/api/v1/books/9')
+      .send({
+        bookName: 'A Game of Thrones',
+        bookISBN: '0553103547',
+      })
+      .end((err, res) => {
+        should.not.exist(err);
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('unsuccessful');
+        res.body.message.should.eql('Book Id invalid');
+        done();
+      });
+  });
+  it('should respond with a 200 no information supplied', (done) => {
+    chai.request(app)
+      .put('/api/v1/books/1')
+      .end((err, res) => {
+        should.not.exist(err);
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('invalid');
+        res.body.message.should.eql('no information supplied');
+        done();
+      });
+  });
+  it('should respond with a 200 success', (done) => {
+    chai.request(app)
+      .put('/api/v1/books/1')
+      .send({
+        bookName: 'A Clash of Kings',
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure errors
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('success');
+        res.body.message.should.eql('Book Details Updated');
+        res.body.data.bookName.should.eql('A Clash of Kings');
+        done();
+      });
+  });
+  it('should respond with a 200 success', (done) => {
+    chai.request(app)
+      .put('/api/v1/books/1')
+      .send({
+        description: 'A tail about the seven kingdoms and all that',
+      })
+      .end((err, res) => {
+        should.not.exist(err);
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('success');
+        res.body.message.should.eql('Book Details Updated');
+        res.body.data.description.should.eql('A tail about the seven kingdoms and all that');
+        done();
+      });
+  });
+  it('should respond with a 200 success', (done) => {
+    chai.request(app)
+      .put('/api/v1/books/1')
+      .send({
+        bookName: 'A Clash of Kings 2',
+        publishYear: '1998',
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure no errors
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('success');
+        res.body.message.should.eql('Book Details Updated');
+        res.body.data.bookName.should.eql('A Clash of Kings 2');
+        res.body.data.publishYear.should.eql('1998-01-01T00:00:00.000Z');
+        done();
+      });
+  });
+  it('should respond with a 200 success', (done) => {
+    chai.request(app)
+      .put('/api/v1/books/2')
+      .send({
+        bookName: 'Harry Potter and the Chamber of Secrets',
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure no errors
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('success');
+        res.body.message.should.eql('Book Details Updated');
+        res.body.data.bookName.should.eql('Harry Potter and the Chamber of Secrets');
+        done();
+      });
+  });
+});
---
+++ test/userversion2.test.js
@@ -0,0 +1,259 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+// import supertest from 'supertest';
+
+import app from '../app';
+
+
+// const app = require('../app');
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+const should = chai.should();
+
+
+chai.use(chaiHttp);
+
+before((done) => {
+  chai.request(app).delete('/api/v2/users').end(done);
+});
+
+describe('POST /api/v2/users/signup version 2', () => {
+  it('should respond with a 200 incomplete details', (done) => {
+    chai.request(app)
+      .post('/api/v2/users/signup')
+      .send({
+        firstname: 'Somebody', // no username or password
+        lastname: 'Else',
+        email: 'somebody@else.com',
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure errors
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('unsuccessful');
+        res.body.error.should.eql('incomplete details');
+        done();
+      });
+  });
+  it('should respond with a 201 Created User on success', (done) => {
+    chai.request(app)
+      .post('/api/v2/users/signup')
+      .send({
+        username: 'nobody',
+        password: 'nobody_password',
+        firstname: 'Somebody',
+        lastname: 'Else',
+        email: 'somebody@else.com',
+        phone: '+234123456789',
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure no errors
+        res.status.should.equal(201);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('User Created');
+        res.body.data.username.should.eql('nobody');
+        res.body.data.Membership.should.eql('Blue');
+        done();
+      });
+  });
+  it('should respond with a 201 Created User on success', (done) => {
+    chai.request(app)
+      .post('/api/v2/users/signup')
+      .send({
+        username: 'somebodyelse',
+        password: 'somebodyGreatPassword',
+        firstname: 'Somebody',
+        lastname: 'Really',
+        email: 'fidelis@thisemail.com',
+        phone: '+23456789101112',
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure no errors
+        res.status.should.equal(201);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('User Created');
+        res.body.data.username.should.eql('somebodyelse');
+        res.body.data.Membership.should.eql('Blue');
+        done();
+      });
+  });
+  // errors
+  it('should respond with a 400 Unique violation', (done) => {
+    chai.request(app)
+      .post('/api/v2/users/signup')
+      .send({
+        username: 'somethingelse',
+        password: 'nobody_password',
+        firstname: 'Somebody',
+        lastname: 'Else',
+        email: 'somebody@else.com', // email is same as before
+        phone: '+234123456789',
+      })
+      .end((err, res) => {
+        should.exist(err); // ensure no errors
+        res.status.should.equal(400);
+        res.type.should.equal('application/json');
+        done();
+      });
+  });
+  it('should respond with a 400 Unique violation', (done) => {
+    chai.request(app)
+      .post('/api/v2/users/signup')
+      .send({
+        username: 'nobody', // username is same as already created
+        password: 'nobody_pasord',
+        firstname: 'Somebody',
+        lastname: 'Else',
+        email: 'somebody@lse.com',
+        phone: '+234123456789',
+      })
+      .end((err, res) => {
+        should.exist(err); // errors present
+        res.status.should.equal(400);
+        res.type.should.equal('application/json');
+        done();
+      });
+  });
+  it('should respond with a 400 Unique violation', (done) => {
+    chai.request(app)
+      .post('/api/v2/users/signup')
+      .send({ // username and email same as before
+        username: 'nobody',
+        password: 'nobody_password',
+        firstname: 'Somebody',
+        lastname: 'Else',
+        email: 'somebody@else.com',
+        phone: '+234123456789',
+      })
+      .end((err, res) => {
+        should.exist(err); // ensure no errors
+        res.status.should.equal(400);
+        res.type.should.equal('application/json');
+        done();
+      });
+  });
+});
+describe('POST /api/v2/users/signin version 2', () => {
+  // incomplete details
+  it('should respond with a 200 no username and password', (done) => {
+    chai.request(app)
+      .post('/api/v2/users/signin')
+      .send({
+        email: 'somebody@else.com',
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure errors
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('no Username and password');
+        done();
+      });
+  });
+  it('should respond with a 200 no password', (done) => {
+    chai.request(app)
+      .post('/api/v2/users/signin')
+      .send({
+        username: 'nobody',
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure no errors
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('no Password');
+        done();
+      });
+  });
+  it('should respond with a 200 no username', (done) => {
+    chai.request(app)
+      .post('/api/v2/users/signin')
+      .send({
+        password: 'nobody_password',
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure no errors
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('no Username');
+        done();
+      });
+  });
+  // valid tests
+  it('should respond with a 202 Success and details', (done) => {
+    chai.request(app)
+      .post('/api/v2/users/signin')
+      .send({
+        username: 'somebodyelse',
+        password: 'somebodyGreatPassword',
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure no errors
+        res.status.should.equal(202);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('success');
+        res.body.data.username.should.eql('somebodyelse');
+        done();
+      });
+  });
+  it('should respond with a 202 Success and details', (done) => {
+    chai.request(app)
+      .post('/api/v2/users/signin')
+      .send({
+        username: 'nobody',
+        password: 'nobody_password',
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure no errors
+        res.status.should.equal(202);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('success');
+        res.body.data.username.should.eql('nobody');
+        done();
+      });
+  });
+  // errors
+  it('should respond with a 200 invalid username (case sensitive)', (done) => { // case sensitive
+    chai.request(app)
+      .post('/api/v2/users/signin')
+      .send({
+        username: 'NoBoDy',
+        password: 'nobody_password',
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure no errors
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('invalid user');
+        done();
+      });
+  });
+  it('should respond with a 200 invalid username (does not accept email)', (done) => { // case sensitive
+    chai.request(app)
+      .post('/api/v2/users/signin')
+      .send({
+        username: 'somebody@else.com',
+        password: 'nobody_password',
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure no errors
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('invalid user');
+        done();
+      });
+  });
+  it('should respond with a 200 invalid username or password', (done) => { // case sensitive
+    chai.request(app)
+      .post('/api/v2/users/signin')
+      .send({
+        username: 'nobody',
+        password: 'wrong_password',
+      })
+      .end((err, res) => {
+        should.not.exist(err); // ensure no errors
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+        res.body.status.should.eql('invalid user or password');
+        done();
+      });
+  });
+});
