6e2f1bf99dc5f2899c138ab53c255797a869e297
Fidelis Ojeah <fidelis.ojeah@gmail.com>
2017-08-19 21:23:32 +0100

    **old tests**

--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ import bodyParser from 'body-parser';
 import http from 'http';
 /*
 import jwt from 'jsonwebtoken';
+*/
 import UserController from './controllers/UserHandlers';
 import BookController from './controllers/Bookhandlers';
-*/
 import userLoginDetails from './controllers/user-controller-v4';
 
 const app = express();
@@ -29,26 +29,26 @@ app.post('/api/v4/users/signup', userLoginDetails.signup);
   // app.post('/api/v1/users/signin', UserController.signin); // signin route would not work atm
   // app.delete('/api/v1/users', UserController.deleteAll);
 */
-/*
-  app.post('/api/v2/users/signin', UserController.login);
-  app.post('/api/v2/users/signup', UserController.signupNew);
-  // signup route with membership, single table
 
-  app.post('/api/v3/users/signup', UserController.signupv3); // New login with email or uswername
-  app.post('/api/v3/users/signin', UserController.loginNew); // New login with email or uswername
+app.post('/api/v2/users/signin', UserController.login);
+app.post('/api/v2/users/signup', UserController.signupNew);
+// signup route with membership, single table
 
-  app.delete('/api/v2/users', UserController.clearTable);
-  app.delete('/api/v1/books', BookController.deleteAllBooks);
+app.post('/api/v3/users/signup', UserController.signupv3); // New login with email or uswername
+app.post('/api/v3/users/signin', UserController.loginNew); // New login with email or uswername
 
-  app.post('/api/v1/users/:userId/books', UserController.borrowBook);
-  app.get('/api/v1/Users/:userId/books', UserController.viewBorrowed)
-    .put('/api/v1/Users/:userId/books', UserController.returnBook);
-  app.post('/api/v1/books', BookController.newBook);
-  app.post('/api/v1/books/mod', BookController.updateBookQuantity);
-  app.get('/api/v1/books', BookController.allBooks);
-  app.put('/api/v1/books/:bookId', BookController.modBook);
-  app.post('/api/v1/authors', BookController.newAuthor);
-*/
+app.delete('/api/v2/users', UserController.clearTable);
+app.delete('/api/v1/books', BookController.deleteAllBooks);
+
+app.post('/api/v1/users/:userId/books', UserController.borrowBook);
+app.get('/api/v1/Users/:userId/books', UserController.viewBorrowed)
+  .put('/api/v1/Users/:userId/books', UserController.returnBook);
+app.post('/api/v1/books', BookController.newBook);
+app.post('/api/v1/books/mod', BookController.updateBookQuantity);
+app.get('/api/v1/books', BookController.allBooks);
+app.put('/api/v1/books/:bookId', BookController.modBook);
+app.post('/api/v1/authors', BookController.newAuthor);
+
 
 app.get('/', (req, res) => res.status(202).send({
   message: 'Welcome to Hello-Books',
