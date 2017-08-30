27b4fd7e9a239e14c1ffd12610c02e4785b5fc33
Fidelis Ojeah <Fidelis@Fideliss-MacBook-Pro.local>
2017-08-30 18:35:38 +0100

    added environmental variables

--- server/index.js
+++ server/index.js
@@ -2,21 +2,24 @@ import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import cookieEncrypter from 'cookie-encrypter';
 import session from 'express-session';
 import http from 'http';
-/*
-import jwt from 'jsonwebtoken';
-*/
+
 import UserController from './controllers/UserHandlers';
 import BookController from './controllers/Bookhandlers';
 import userLoginDetails from './controllers/user-controller-v4';
 
 const app = express();
 
+const jsonSecretKey = process.env.JSONWEB_SECRET;
+const cookieSecretKey = process.env.COOKIE_SECRET_KEY;
+const sessionSecretKey = process.env.SESSION_SECRET_KEY;
+
 // log requests
 app.use(logger('dev'));
 // Set JWT secret
-app.set('JsonSecret', 'myJS0NSecretHell0Bo0ksWh@t3v3r');
+app.set('JsonSecret', jsonSecretKey);
 app.use(bodyParser.urlencoded({
   extended: false,
 }));
@@ -24,16 +27,25 @@ app.use(bodyParser.json({
   type: 'application/json',
 }));
 // for cookies
-app.use(cookieParser());
+app.use(cookieParser(cookieSecretKey));
+app.use(cookieEncrypter(cookieSecretKey));
 
-app.use(session({}));
+app.use(session({
+  key: 'user_id',
+  secret: sessionSecretKey,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: 600000,
+  },
+}));
 
 app.post('/api/v4/users/signup', userLoginDetails.signup);
 app.get('/api/v4/users/verify', userLoginDetails.activateUser);
 
-
 app.delete('/api/v2/users', UserController.clearTable);
 app.delete('/api/v1/books', BookController.deleteAllBooks);
+
 
 app.get('/', (req, res) => res.status(202).send({
   message: 'Welcome to Hello-Books',
