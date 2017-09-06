91c148771d84b0d36c053629819bf52ce02e3529
Fidelis Ojeah <Fidelis@Fidelis-MacBook-Pro.local>
2017-09-06 18:48:52 +0100

    created route to update book quantity

--- server/controllers/books-controller-v4.js
+++ server/controllers/books-controller-v4.js
@@ -47,6 +47,109 @@ class bookProps {
         });
       });
   }
+  static viewBooks(req, res) {
+    const bookID = req.query.id || null;
+
+    if (bookID === null) { // for all books
+      Books
+        .findAll({})
+        .then((allBooks) => {
+          if (allBooks === null || allBooks.length === 0) { // if no book is found
+            res.status(200).json({
+              status: 'Unsuccessful',
+              message: 'No Books',
+            });
+          } else {
+            res.status(202).json({
+              status: 'Successful',
+              data: allBooks,
+            });
+          }
+        })
+        .catch(error => res.status(500).json(error)); // catch error from findall
+    } else {
+      /*
+      Books
+        .findOne({
+
+        })
+        .then((bookInfo) => { })
+        .catch();
+        */
+    }
+  }
+  static newBook(req, res) {
+    checkSession
+      .checkAdmin(req.decoded)
+      .then(() => {
+        const bookQuant = req.body.quantity || 1;
+        const bookImg = req.body.image || null;
+        const pubYear = req.body.publishyear || null;
+        const bookname = req.body.bookname || null;
+        const ISBN = req.body.ISBN || null;
+        const desc = req.body.description || null;
+        const authorId = req.body.authorIds || 1; // author or anonymous
+
+      })
+      .catch((error) => {
+        res.status(401).json({
+          status: 'Unsuccessful',
+          message: error,
+        });
+      });
+  }
+  static getAuthors(req, res) {
+
+  }
+  static updateBookQuantity(req, res) {
+    checkSession
+      .checkAdmin(req.decoded)
+      .then(() => {
+        const bookId = parseInt(req.body.Id, 10);
+        const bookQuantity = parseInt(req.body.quantity, 10);
+        if (!isNaN(bookId) || !isNaN(bookQuantity)) { // has to be a number really
+          Books
+            .findOne({ // search for book with id
+              where: {
+                id: bookId,
+              },
+            })
+            .then((bookDetails) => {
+              if (bookDetails === null) { // if a book is not found
+                res.status(400).json({
+                  status: 'Unsuccessful',
+                  message: 'No book found',
+                });
+              } else { // if bookDetails is set then add
+                bookDetails.update({
+                  bookQuantity:
+                  (bookDetails.bookQuantity + bookQuantity) < 1 ? 1 :
+                    (bookDetails.bookQuantity + bookQuantity),
+                  // Never less than 1
+                })
+                  .then(
+                  addBook => res.status(200).json({
+                    status: 'Success',
+                    message: 'Book Updated Successfully',
+                    data: addBook,
+                  }))
+                  .catch(error => res.status(500).send(error));
+              }
+            });
+        } else {
+          res.status(400).json({
+            status: 'Unsuccessful',
+            message: 'Invalid Book ID',
+          });
+        }
+      })
+      .catch((error) => {
+        res.status(401).json({
+          status: 'Unsuccessful',
+          message: error,
+        });
+      });
+  }
   static
 }
 
