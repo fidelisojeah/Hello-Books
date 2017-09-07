36591c2227654892029a134dda9fa20438c58a2d
Fidelis Ojeah <Fidelis@Fidelis-MacBook-Pro.local>
2017-09-07 23:02:02 +0100

    created routes for borrowing books

--- server/controllers/user-books-controller.js
+++ server/controllers/user-books-controller.js
@@ -20,6 +20,87 @@ class userBookInteraction {
       }
     });
   }
+  static viewBorrowedBook(req, res) {
+    const userId = parseInt(req.params.userId, 10);
+    const isReturned = req.query.returned || null;
+
+    userBookInteraction.validateEntry(userId, 100)
+      .then((validated) => {
+        if (validated === 'Valid') {
+          if (userId !== req.decoded.userId) { // false user?
+            res.status(401).json({
+              status: 'Unsuccessful',
+              message: 'Not Allowed',
+            });
+          } else {
+            UserDetails
+              .findOne({
+                where: {
+                  id: userId,
+                  isActive: true,
+                },
+              })
+              .then((userLoginDetails) => {
+                if (!userLoginDetails) {
+                  res.status(401).json({
+                    status: 'Unsuccessful',
+                    message: 'Not Allowed',
+                  });
+                } else { // if User found
+                  // find all BookLendings with User Id
+                  const returnedSearch = (isReturned === 'false') ? {
+                    userId: userLoginDetails.id,
+                    actualReturnDate: null,
+                  } : { userId: userLoginDetails.id };
+                  BookLendings
+                    .findAll({
+                      where:
+                      returnedSearch,
+                      include: [{
+                        model: Books,
+                        attributes: ['bookName', 'bookISBN', 'bookImage', 'publishYear'],
+                        where: {
+                          isActive: true,
+                        },
+                      }],
+                      attributes: [
+                        'id',
+                        'borrowDate',
+                        'dueDate',
+                        'actualReturnDate',
+                      ],
+                    })
+                    .then((bookLends) => {
+                      if (bookLends) {
+                        res.status(202).json({
+                          status: 'Success',
+                          data: bookLends,
+                        });
+                      } else {
+                        res.status(200).json({
+                          status: 'Unsuccessful',
+                          message: 'No borrowed Books',
+                        });
+                      }
+                    })
+                    .catch(error => res.status(501).send(error));
+                }
+              })
+              .catch(error => res.status(501).send(error));
+          }
+        } else {
+          res.status(501).json({
+            status: 'Unsuccessful',
+            message: 'Try again!??',
+          });
+        }
+      })
+      .catch(error =>
+        res.status(404).json({
+          status: 'Unsuccessful',
+          message: error,
+        }));
+  }
   static borrowBook(req, res) {
     const userId = parseInt(req.params.userId, 10);
     const bookId = parseInt(req.body.bookId, 10); // change case
