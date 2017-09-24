6718175cacfea32c4d439963bed7e354e561ebdc
Fidelis Ojeah <fidelis.ojeah@gmail.com>
2017-09-24 12:07:08 +0100

    Minor fixes

--- client/src/components/actions/newBookAction.js
+++ client/src/components/actions/newBookAction.js
@@ -1,40 +1,32 @@
 import request from 'superagent';
+import axios from 'axios';
 
 import {
   CLOUDINARY_CLOUD_URL,
   CLOUDINARY_UPLOAD_PRESET
 } from './types';
 
-const config = {
-  headers: {
-    'X-Requested-With': 'XMLHttpRequest'
-  },
-  onUploadProgress(progressEvent) {
-    // Do whatever you want with the native progress event
-    // console.log('progressEvent', progressEvent);
-    const progress =
-      Math.round((progressEvent.loaded * 100.0) / progressEvent.total);
-    console.log(progressEvent, '----');
-    // document.getElementById('progress').style.width = `${progress} %`;
-
-    console.log(`onUploadProgress progressEvent.loaded: ${progressEvent.loaded},
-    progressEvent.total: ${progressEvent.total}`);
-  }
-};
 
 /**
  * @description
- * @param {*} Data
- * @return {*} return data
+ * @param {file} Data
+ * @return {promise} return data
  */
-export function bookImageUpload(Data) {
+export function bookImageUpload(imageData) {
   return dispatch => {
-    // const fd = new FormData();
-    // fd.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
-    // fd.append('file', Data);
     return request
       .post(CLOUDINARY_CLOUD_URL)
       .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
-      .field('file', Data);
+      .field('file', imageData);
+  };
+}
+/**
+ * @description
+ * @param {object} bookData
+ * @return {promise} return Data
+ */
+export function newBookRequest(bookData) {
+  return dispatch => {
+    return axios.post('/api/v4/books', bookData);
   };
 }
