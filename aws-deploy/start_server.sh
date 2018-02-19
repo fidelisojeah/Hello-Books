e929177a0cbd04194cb7a7451dfb591b8b2e495a
Fidelis Ojeah <fidelis.ojeah@gmail.com>
2018-02-19 19:25:58 +0100

    updates to appspec file  -   integration with AWS for CI/CD

---
+++ aws-deploy/start_server.sh
@@ -0,0 +1,5 @@
+# aws-deploy/start_server
+#!/bin/bash
+source /home/ec2-user/.bash_profile
+cd /home/ec2-user/hello-books/Hello-Books
+pm2 start server/index.js --interpreter ./node_modules/.bin/babel-node --name "Hello-Books"
---
+++ aws-script.sh
@@ -0,0 +1,5 @@
+export NODE_ENV=production
+npm install
+
+npm run build:webpack
+npm run build:docs
