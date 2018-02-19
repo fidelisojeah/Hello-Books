9ab50198bed04c2efe906964614c3087ad662d8a
Fidelis Ojeah <fidelis.ojeah@gmail.com>
2018-02-19 20:04:20 +0100

    new updates and all that

--- aws-deploy/start_server.sh
+++ aws-deploy/start_server.sh
@@ -3,3 +3,4 @@
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/hello-books/Hello-Books
 pm2 start server/index.js --interpreter ./node_modules/.bin/babel-node --name "Hello-Books"
+sudo service nginx start
--- aws-deploy/stop_server.sh
+++ aws-deploy/stop_server.sh
@@ -3,3 +3,4 @@
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/hello-books/Hello-Books
 pm2 stop all
+sudo service nginx stop
