3e1595ecf7d0bf69fd745be7133721bcf7bd639c
Fidelis Ojeah <fidelis.ojeah@gmail.com>
2018-03-11 11:36:51 +0100

    webpack edit

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -25,7 +25,6 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
-    new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
       minimize: true,
       compress: {
