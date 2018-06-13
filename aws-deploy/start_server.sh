142e8ccaa2e6ab0cd41b8963fee075875986f1e3
Fidelis Ojeah <fidelis_oj@yahoo.co.uk>
2018-06-13 12:26:44 +0100

    Update start_server.sh

--- aws-deploy/start_server.sh
+++ aws-deploy/start_server.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/hello-books
-pm2 start server/index.js --interpreter ./node_modules/.bin/babel-node --name "Hello-Books"
+pm2 start /home/ec2-user/hello-books/server/index.js --interpreter /home/ec2-user/hello-books/node_modules/.bin/babel-node --name "Hello-Books"
 sudo service nginx start
