a3af546d2d2bf4c7a3e3d2737b616a786f163bf4
Fidelis Ojeah <fidelis_oj@yahoo.co.uk>
2018-06-13 10:33:21 +0100

    Update HelloBooksSendMail.js

--- server/helpers/HelloBooksSendMail.js
+++ server/helpers/HelloBooksSendMail.js
@@ -34,6 +34,9 @@ class HelloBooksSendMail {
           auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASSWORD
+          },
+          tls:{
+            ciphers:'SSLv3'
           }
       });
       
